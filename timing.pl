fe6997d676bb9c9515ad6a07c8c2ccaaed79cd26
perlino <ivanchenko.michael.work@gmail.com>
2012-09-03 12:48:19 +0200

    time pieces of code

---
+++ timing.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use Time::HiRes qw( gettimeofday tv_interval );
+
+my $t0 = [gettimeofday];
+slow_function();
+print tv_interval( $t0 ) . " sec\n";
+
+sub slow_function
+{
+	print "Type something: ";
+	my $data = <>;
+}
