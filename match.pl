3fce8afecd7de65f9738f0b6b4a52cedf120519a
perlino <ivanchenko.michael.work@gmail.com>
2012-10-25 12:26:11 +0300

    count substrings in a string

--- match.pl
+++ match.pl
@@ -3,16 +3,22 @@
 $string = 'abcdefg efgdabcz';
 
 $matched = $string =~ /ab.{2}/g;
+
 ($match) = $string =~ /ab.{2}/g;
 ($match1, $match2) = $string =~ /ab.{2}/g;
 @matches = $string =~ /ab.{2}/g;
-$count = @matches = $string =~ /ab.{2}/g;
+
+$count  = @matches = $string =~ /ab.{2}/g;
+$count2 = $string =~ s{abc}{abc}g;
 
 print "matched: [$matched]\n";
+
 print "match: [$match]\n";
 print "match1: [$match1], match2: [$match2]\n";
 print "matches: [@matches]\n";
-print "count: [$count]\n";
+
+print "count:  [$count]\n";
+print "count2: [$count2]\n";
 
 ## matches word with the first char the same as the last one
 #$string =~ /^(.{1}).*\1$/
