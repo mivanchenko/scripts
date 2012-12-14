7d864dd6a0fda1cfffced6b1e688a4759c77391f
perlino <ivanchenko.michael.work@gmail.com>
2012-12-14 19:18:10 +0200

    recurse dir

--- dir.pl
+++ dir.pl
@@ -1,20 +1,32 @@
 #!/usr/bin/env perl
 
+
+use File::Find;
+find( sub { /p[lm]$/ && print "$_\n" }, '.' );
+
+
 use Path::Class;
 
-sub _read_filepaths
-{
-    my $self = shift;
-    my $path = shift;
-    my @filepaths;
-
-    my $dir = dir( split( '/', $path ) );
-
-    while ( my $file = $dir->next )
-    {
-        next if $file->is_dir();
-        push @filepaths, $file->stringify();
+my @files;
+dir('.')->recurse( callback => sub {
+    my $file = shift;
+    if ( $file =~ /p[lm]$/ ) {
+        push @files, $file->absolute->stringify;
     }
+});
+print Dumper \@files;
 
-    return \@filepaths;
+
+sub _read_filepaths {
+	my ($self, $path) = @_;
+	my @filepaths;
+
+	my $dir = dir( split( '/', $path ) );
+
+	while ( my $file = $dir->next ) {
+		next if $file->is_dir();
+		push @filepaths, $file->stringify();
+	}
+
+	return \@filepaths;
 }
