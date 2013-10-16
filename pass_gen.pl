fcad4579e128f44535f4119784b69a9615cc5b2f
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2013-10-16 12:06:52 +0300

    More sources for passwords

--- pass_gen.pl
+++ pass_gen.pl
@@ -1,18 +1,19 @@
 #!/usr/bin/env perl
 
-my $uid = gen_pass( 16 );
+my $uid = gen_pass(12);
 print "$uid\n";
 
-sub gen_pass
-{
+sub gen_pass {
 	my $length = shift;
-	my $pool = 'abcdefghijkmnpqrstuvwxyz23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
-#	my $pool = '0123456789';
 	my $pass;
+	my $al      = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	my $num     = '0123456789';
+	my $special = '-=~!@#%$^&*()_+[]{}<>';
 
-	while ( length( $pass ) < $length )
-	{
-		$pass .= substr( $pool, (int(rand(length($pool)))), 1 );
+	my $pool    = $al.$num.$special;
+
+	while ( length($pass) < $length ) {
+		$pass .= substr($pool, (int(rand(length($pool)))), 1);
 	}
 
 	return $pass;
